dd42f5f66263c46d331936cefa4ac3b869fc69e4
Riccardo Nestola <riccardinux@gmail.com>
2023-12-01 17:14:13 +0100

    add carousel - fix <a> tag

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,25 @@
-import { useState } from 'react'
 
+import Carousel from './Components/Carousel/Carousel'
+import images from './data/images'
+import {Header} from './Components/Header/Header'
+import CardList from './Components/Cards/CardList'
+import Footer from './Components/Footer/Footer'
 function App() {
 
 
   return (
     <>
+      <Header />
       
+
+      <main>
+        <Carousel images={images} />
+        <CardList />
+      </main>
+
+      <footer className='text-gray-600 body-font'>
+        <Footer />
+      </footer>
     </>
   )
 }
--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -6,16 +6,16 @@ export const Header = () => {
 
             <header className="text-gray-600 body-font">
                 <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
-                    <a href='' className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
+                    <div className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
                         <svg xmlns="http://www.w3.org/2000/svg" height="32" width="32" viewBox="0 0 512 512">
                             <path d="M192 32c0 17.7 14.3 32 32 32c123.7 0 224 100.3 224 224c0 17.7 14.3 32 32 32s32-14.3 32-32C512 128.9 383.1 0 224 0c-17.7 0-32 14.3-32 32zm0 96c0 17.7 14.3 32 32 32c70.7 0 128 57.3 128 128c0 17.7 14.3 32 32 32s32-14.3 32-32c0-106-86-192-192-192c-17.7 0-32 14.3-32 32zM96 144c0-26.5-21.5-48-48-48S0 117.5 0 144V368c0 79.5 64.5 144 144 144s144-64.5 144-144s-64.5-144-144-144H128v96h16c26.5 0 48 21.5 48 48s-21.5 48-48 48s-48-21.5-48-48V144z" /></svg>
                         <a href="/" className="ml-3 text-xl">Il Mio Blog</a>
-                    </a>
+                    </div>
                     <nav className="md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-400	flex flex-wrap items-center text-base justify-center">
-                        <a className="mr-5 hover:text-gray-900">Nuovo Post</a>
-                        <a className="mr-5 hover:text-gray-900">Tags</a>
-                        <a className="mr-5 hover:text-gray-900">Third Link</a>
-                        <a className="mr-5 hover:text-gray-900">Fourth Link</a>
+                        <a href='#' className="mr-5 hover:text-gray-900">Nuovo Post</a>
+                        <a href='#' className="mr-5 hover:text-gray-900">Tags</a>
+                        <a href='#' className="mr-5 hover:text-gray-900">Third Link</a>
+                        <a href='#' className="mr-5 hover:text-gray-900">Fourth Link</a>
                     </nav>
                     
                 </div>
