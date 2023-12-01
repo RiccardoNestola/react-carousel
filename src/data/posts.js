4f645b6e5745f176f8aefda7fa5b8535130aafd9
Riccardo Nestola <riccardinux@gmail.com>
2023-12-01 17:22:48 +0100

    add more posts

--- src/data/posts.js
+++ src/data/posts.js
@@ -11,7 +11,7 @@
     {
         id: 2,
         title: "Titolo del Post",
-        image: "https://source.unsplash.com/random/720×400?night",
+        image: "https://source.unsplash.com/random/720×400?mountain",
         content:
             "Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit animi unde quasi enim non esse ratione voluptas voluptate, officiis veritatis magni blanditiis possimus nobis cum id inventore corporis deserunt hic.",
         tags: ["js", "css"],
@@ -29,12 +29,56 @@
     {
         id: 4,
         title: "Titolo del Post",
-        image: "https://source.unsplash.com/random/720×400",
+        image: "https://source.unsplash.com/random/720×400?tree",
         content:
             "Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit animi unde quasi enim non esse ratione voluptas voluptate, officiis veritatis magni blanditiis possimus nobis cum id inventore corporis deserunt hic.",
         tags: ["html"],
         published: false,
     },
+    {
+        id: 5,
+        title: "Titolo del Post",
+        image: "https://source.unsplash.com/random/720×400?sky",
+        content:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit animi unde quasi enim non esse ratione voluptas voluptate, officiis veritatis magni blanditiis possimus nobis cum id inventore corporis deserunt hic.",
+        tags: ["html", "css"],
+        published: true,
+    },
+    {
+        id: 6,
+        title: "Titolo del Post",
+        image: "https://source.unsplash.com/random/720×400?animal",
+        content:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit animi unde quasi enim non esse ratione voluptas voluptate, officiis veritatis magni blanditiis possimus nobis cum id inventore corporis deserunt hic.",
+        tags: ["html", "css"],
+        published: true,
+    },
+    {
+        id: 7,
+        title: "Titolo del Post",
+        image: "https://source.unsplash.com/random/720×400?night",
+        content:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit animi unde quasi enim non esse ratione voluptas voluptate, officiis veritatis magni blanditiis possimus nobis cum id inventore corporis deserunt hic.",
+        tags: ["js", "css"],
+        published: true,
+    },
+    {
+        id: 8,
+        title: "Titolo del Post",
+        image: "https://source.unsplash.com/random/720×400?house",
+        content:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit animi unde quasi enim non esse ratione voluptas voluptate, officiis veritatis magni blanditiis possimus nobis cum id inventore corporis deserunt hic.",
+        tags: ["js", "php"],
+        published: true,
+    },
+    {
+        id: 9,
+        title: "Titolo del Post",
+        image: "https://source.unsplash.com/random/720×400?car",
+        content:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit animi unde quasi enim non esse ratione voluptas voluptate, officiis veritatis magni blanditiis possimus nobis cum id",
+            tags: ["js", "php"],
+        published: true,}
 ];
 
 export default posts;
